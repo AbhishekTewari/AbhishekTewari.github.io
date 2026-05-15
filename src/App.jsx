3fd49ebf1b65d099a40a45c68da0110e463f7981
AtiwariPaktolus <atiwari@paktolus.com>
2026-05-15 12:58:06 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import Header from "./components/Header/Header";
+import HeroBanner from "./components/HeroBanner/HeroBanner";
+import Skills from "./components/Skills/Skills";
+import Project from "./components/Project/Project";
+import Experience from "./components/Experience/Experience";
+import Footer from "./components/Footer/Footer";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
-    <div>Hello, World!</div>
+   <div className="App">
+      <Header />
+      {/* <HeroBanner />
+      <Skills />
+      <Project /> 
+      <Experience />
+      <Contact />
+      <Footer /> */}
+   </div>
   );
 }
 
